96c79fa7b3c2b80f6cfe179e9b0d862baeeaf095
juan fuentes <jfuentes.dev@gmail.com>
2020-02-17 01:02:35 -0300

    Primer commit

---
+++ lugar/lugar.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+
+const getLugarLatLng = async ( dir ) => {
+
+     const encodedUrl = encodeURI( dir );
+     
+     const instance = axios.create({
+          baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedUrl}`,
+          headers: {'x-rapidapi-key': 'b425ed27bbmsh6482b8b398f94d8p1f00c9jsnc98f6a9eeb5e'}
+     });
+     
+     const resp = await instance.get();
+     
+     if ( resp.data.Results.length === 0 ) {
+          throw new Error(`No hay resultados para ${ dir }`);
+     } 
+
+     const data       = resp.data.Results[0];
+     const direccion  = data.name;
+     const lat = data.lat;
+     const lng = data.lon;
+
+     return {
+          direccion,
+          lat,
+          lng
+     }
+}
+
+
+module.exports = {
+     getLugarLatLng
+}
+
